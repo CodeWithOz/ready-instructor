71908ad43ff5055bed60e1db58c7da6ff5459339
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-12 13:49:34 +0100

    enhance(core): add cors support

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(cors);
 
 console.log('starting server');
+const port = process.env.PORT || 5000;
 app.listen(port);
