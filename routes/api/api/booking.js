61390ca91594c2dd320b1dfbc165fa424ca6cc18
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-12 15:33:49 +0100

    feat(api): create an endpoint for creating bookings

--- routes/api/api/booking.js
+++ routes/api/api/booking.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
+const validator = require('validator').default;
 const Booking = require('../../../models/Booking');
+const { logger } = require('../../../helpers');
 
 router.get('/:bookingId', (req, res, next) => {
     const { bookingId } = req.params;
@@ -12,3 +15,55 @@ router.get('/:bookingId', (req, res, next) => {
         })
         .catch(next);
 });
+
+router.post(
+    '/',
+    [
+        body('title').trim().notEmpty().escape(),
+        body('description').trim().notEmpty().escape(),
+        body('duration').isNumeric(),
+        body('start').isNumeric(),
+        body('bookerId').isMongoId(),
+        body('bookedEducators')
+            .isArray()
+            .custom(bookedEducators =>
+                bookedEducators.every(educatorId =>
+                    validator.isMongoId(educatorId)
+                )
+            ),
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            logger(
+                'validation errors at POST /booking:',
+                JSON.stringify(errors.array())
+            );
+            return next(errors);
+        }
+        next();
+    },
+    (req, res, next) => {
+        const {
+            title,
+            description,
+            duration,
+            start,
+            bookerId,
+            bookedEducators,
+        } = req.body;
+        Booking.create({
+            title,
+            description,
+            duration,
+            start,
+            bookerId,
+            bookedEducators,
+        })
+            .then(booking => {
+                logger('newly created booking:', booking);
+                res.status(200).json({ booking });
+            })
+            .catch(next);
+    }
+);
