57743e72d066fde1513810f6882512d0b3c4e02a
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-12 18:04:13 +0100

    fix(core): configure the cors middleware properly to fix failure to handle requests

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const isProd = process.env.NODE_ENV === 'production';
 
-app.use(cors);
+app.use(cors());
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
